6966f4f202d9732b3e8ccc76b4170020e806c462
githubofuttam <uttamkumar9234567@gmail.com>
2024-10-27 11:16:29 +0530

    variables

--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
-console.log("Learn Javascript");
+const accountId = 144553;
+let accountEmail = "uttam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId  2
+
+accountEmail = "hc@hc.com"
+accountPassword = "4644366"
+accountCity = "Chitrakoot"
+
+
+console.table([accountId , accountEmail, accountPassword, accountCity, accountState])
